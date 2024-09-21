9d78718cb48981abd7965b762adc98c01a3065ec
giljjong <doriyet@naver.com>
2024-09-22 00:09:51 +0900

    스프링 DB 2편섹션 3

--- itemservice-db/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
+++ itemservice-db/src/test/java/hello/itemservice/ItemServiceApplicationTests.java
@@ -3,6 +3,7 @@ package hello.itemservice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// 해당 애노테이션을 쓰면 @SpringBootApplication을 찾아 간다.
 @SpringBootTest
 class ItemServiceApplicationTests {
 
--- itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
+++ itemservice-db/src/test/java/hello/itemservice/domain/ItemRepositoryTest.java
@@ -5,19 +5,38 @@ import hello.itemservice.repository.ItemSearchCond;
 import hello.itemservice.repository.ItemUpdateDto;
 import hello.itemservice.repository.memory.MemoryItemRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@Transactional
 @SpringBootTest
 class ItemRepositoryTest {
 
     @Autowired
     ItemRepository itemRepository;
+
+    @Autowired
+    PlatformTransactionManager transactionManager;
+    TransactionStatus status;
+
+/*
+    @BeforeEach
+    void beforeEach() {
+        // 트랜잭션 시작
+        status = transactionManager.getTransaction(new DefaultTransactionDefinition());
+    }
+*/
 
     @AfterEach
     void afterEach() {
@@ -26,8 +45,12 @@ class ItemRepositoryTest {
         if (itemRepository instanceof MemoryItemRepository) {
             ((MemoryItemRepository) itemRepository).clearStore();
         }
+        // 트랜잭션 롤백
+        // transactionManager.rollback(status);
     }
 
+    // @Commit // 해당 애노테이션을 붙이면 Transaction을 걸지만 그래도 커밋하겠다는 의미 @Rollback(value=false)와 동일한 의미
+    // @Transactional
     @Test
     void save() {
         //given
