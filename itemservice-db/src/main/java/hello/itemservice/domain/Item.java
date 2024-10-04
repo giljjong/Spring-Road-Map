a409496778bca34c2fd999efb56f72b3a61f63ee
giljjong <doriyet@naver.com>
2024-10-05 00:58:31 +0900

    스프링 DB 2편섹션 5

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,11 +2,19 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+// JPA는 public 또는 protected의 기본 생성자가 필수
+// @Table(name = "Item") 객체 명과 같으면 생략 가능
 public class Item {
 
+    // @Id 테이블의 PK와 해당 필드를 매핑
+    // @GeneratedValue PK 생성 값을 데이터베이스에서 생성하는 IDENTITY 방식을 사용
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Column(name = "item_name", length = 10)
     private String itemName;
     private Integer price;
     private Integer quantity;
